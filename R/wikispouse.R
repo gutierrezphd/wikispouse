796aa93e8f4f5e1dd2a87a5c15ba57bf37e43653
Ian Gutierrez <ian.gutierrez.phd@gmail.com>
2020-12-25 17:53:39 -0500

    Initial commit

---
+++ R/wikispouse.R
@@ -0,0 +1,48 @@
+### WIKISPOUSE FUNCTON ####
+# ian.gutierrez.phd@gmail.com
+# @ianagutierrez
+
+wikispouse <- function() {
+  require(rvest)
+  require(stringi)
+  require(tidyverse)
+  require(magrittr)
+  require(tools)
+  name.space <- readline("Name: ")
+  name.space %<>% tools::toTitleCase(.)
+  name <- gsub(" ","_", name.space)
+  url <- paste("https://en.wikipedia.org/wiki/", name, sep="")
+  site <-  url %>%
+    read_html() %>%
+    html_nodes(xpath = '//*[@id="mw-content-text"]/div/table[1]') %>%
+    html_table(fill = TRUE)
+  site <- t(site[[1]])
+  site %<>% as.data.frame
+  ifelse(lapply(site[1,], function(x) grepl("Spouse", x)) %>%
+           unlist %>%
+           as.vector %>%
+           any(.)==TRUE,
+         spouses <- site[[lapply(site[1,], function(x) grepl("Spouse", x)) %>% unlist %>% grep("TRUE", .)]][2],
+         stop("No spouses indexed for this wikipedia record")
+  )
+  spouses %<>% gsub("\\[[^\\]]*\\]", "NA", ., perl=TRUE) #remove footnotes in brackets, revisit this
+  spouses %<>% strsplit(., '[();]')
+  spouses %<>% unlist
+  length(spouses) <- ceiling(length(spouses)/3)*3
+  spouses %<>% split(., seq(1:3)) %>% as.data.frame
+  names(spouses) <- c("spouse", "married", "divorce/death")
+  spouses$spouse %<>% gsub("\n","",.)
+  spouses$spouse %<>% gsub("^\\s+|\\s+$", "", .)
+  spouses$spouse %<>% as.character(.)
+  spouses$married %<>% gsub("[^0-9-]", "", .)
+  spouses$`divorce/death` %<>% gsub("[^0-9-]", "", .)
+  spouses$married %<>% as.numeric(.)
+  spouses$`divorce/death` %<>% as.numeric(.)
+  cat("\n", name.space, "\n", sep="")
+  return(spouses)
+} #create the function
+
+#run this code, find some famous spouses
+#wikispouse()
+
+
