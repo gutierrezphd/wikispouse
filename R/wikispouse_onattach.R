af4a0fac5ca136d46a2b1c9604d9fc8ac4775b75
Ian Gutierrez <ian.gutierrez.phd@gmail.com>
2021-01-08 19:54:57 -0500

    Update wikispouse_onattach.R

--- R/wikispouse_onattach.R
+++ R/wikispouse_onattach.R
@@ -4,6 +4,6 @@
   message <- c("\n Thank you for using the {wikispouse} R package!",
                "\n \n Have fun looking up spouses of famous people, if that's your thing",
                "\n \n You can contact me at ian.gutierrez.phd@gmail.com or @ianagutierrez",
-               "\n \n Happy searching!")
+               "\n \n Happy searching! \n")
   packageStartupMessage(message)
 }
