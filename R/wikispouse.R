74b057bca26af21b450622de6da249a0c0dd5a47
Ian Gutierrez <ian.gutierrez.phd@gmail.com>
2021-01-08 19:54:15 -0500

    Create .onAttach file

--- R/wikispouse.R
+++ R/wikispouse.R
@@ -2,14 +2,6 @@
 # ian.gutierrez.phd@gmail.com
 # @ianagutierrez
 
-.onAttach <- function(libname, pkgname) {
-  message <- c("\n Thank you for using the {wikispouse} R package!",
-               "\n \n Have fun looking up spouses of famous people, if that's your thing",
-               "\n \n You can contact me at ian.gutierrez.phd@gmail.com or @ianagutierrez",
-               "\n \n Happy searching!")
-  packageStartupMessage(message)
-}
-
 wikispouse <- function() {
   require(rvest)
   require(stringi)
