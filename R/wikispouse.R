0ac2e38a60494c0b02dccd363b0a4d8b54b6a7f4
Ian Gutierrez <ian.gutierrez.phd@gmail.com>
2021-01-08 19:45:00 -0500

    Update wikispouse.R

--- R/wikispouse.R
+++ R/wikispouse.R
@@ -2,7 +2,7 @@
 # ian.gutierrez.phd@gmail.com
 # @ianagutierrez
 
-.onAttach <- function(gutierrezphd, wikispouse) {
+.onAttach <- function(libname, pkgname) {
   message <- c("\n Thank you for using the {wikispouse} R package!",
                "\n \n Have fun looking up spouses of famous people, if that's your thing",
                "\n \n You can contact me at ian.gutierrez.phd@gmail.com or @ianagutierrez",
